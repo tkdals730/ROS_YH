56acbe2c44357b6ede50dc424d018dc6fa6d03e8
tkdals730 <asddddddddddd30@gmail.com>
2026-03-04 19:54:59 -0800

    feat : client와 service 구현

---
+++ beginner_tutorials/scripts/string_length_server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from beginner_tutorials.srv import StringLength, StringLengthResponse
+
+def handle_length(req):
+
+    result = len(req.text)
+
+    rospy.loginfo("요청: '%s' → 길이: %d", req.text, result)
+
+    return StringLengthResponse(result)
+
+def server():
+
+    rospy.init_node('string_length_server')
+
+    rospy.Service("string_length", StringLength, handle_length)
+
+    rospy.loginfo("문자열 길이 서버 준비 완료")
+
+    rospy.spin()
+
+if __name__ == '__main__':
+
+    server()
